b5f92adfe1c1872de3ae7855511180b5d4dc7166
Jonathan Willian <jonathan.willian.tod@gmail.com>
2022-02-09 08:57:44 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,226 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { Table, Column, HeaderCell, Cell } from 'rsuite-table';
+import 'rsuite-table/dist/css/rsuite-table.css';
+
+import dataGenerator from './data';
+
+const chartData = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
+const WalletCell = ({ rowData, ...props }) => (
+  <Cell {...props}>
+    <a target="_blank" rel="noreferrer" href={`https://bscscan.com/token/0xD4d03e510b382244B2289637B5f7D9067bcAaE85?a=${rowData.wallet}`}>{rowData.wallet}</a>
+  </Cell>
+);
+
+const LFCell = ({ rowData, ...props }) => (
+  <Cell {...props}>
+    <div>{rowData.totalMined.toLocaleString('pt')} LF</div>
+  </Cell>
+);
 
 function App() {
+  const [data, setData] = useState({});
+  const [nftQuantity, setNFTQuantity] = useState([]);
+  const [lfQuantity, setLFQuantity] = useState([]);
+  const [fiveStars, setFiveStars] = useState([]);
+  const [fourStars, setFourStars] = useState([]);
+
+  useEffect(() => {
+    const newData = dataGenerator();
+    setData(newData);
+    setNFTQuantity(
+      newData.sortedByWalletNFTQuantity
+        .slice(0, 50)
+        .map((item, index) => ({ id: index + 1, wallet: item.wallet, miners: item.miners.length }))
+    );
+    setLFQuantity(
+      newData.sortedByMinedQuantity
+        .slice(0, 50)
+        .map((item, index) => ({ id: index + 1, wallet: item.wallet, totalMined: item.totalMined }))
+    );
+
+    setFiveStars(
+      newData.sortedByNFTRarityQuantity
+        .slice(0, 10)
+        .map((item, index) => ({ id: index + 1, wallet: item.wallet, miners: item.fiveStar }))
+    );
+    setFourStars(
+      newData.sortedByNFTRarityQuantity
+        .sort((a, b) => b.fourStar - a.fourStar)
+        .slice(0, 10)
+        .map((item, index) => ({ id: index + 1, wallet: item.wallet, miners: item.fourStar }))
+    );
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{ width: '100%', height: '100%' }}>
+      <h1>ELEF Dashboard</h1>
+
+      <div>
+        <h2>Número de jogadores</h2>
+        <p>{data.numberOfPlayers}</p>
+      </div>
+
+      <div>
+        <h2>Número de NFTs</h2>
+        <p>{data.numberOfNFTs}</p>
+      </div>
+
+      <div>
+        <h2>Número de jogadores com 10 NFTs ou mais</h2>
+        <p>{data.numberOfFullLandWallets}</p>
+      </div>
+
+      <div>
+        <h2>Total de LF minado</h2>
+        <p>{data.totalMinedLF} LF</p>
+      </div>
+
+      <div>
+        <h2>Top 50 Carteiras por número de NFTs</h2>
+        <Table data={nftQuantity} width={650} height={400}>
+          <Column width={70} resizable>
+            <HeaderCell>Rank</HeaderCell>
+            <Cell dataKey="id" />
+          </Column>
+
+          <Column width={430} fixed resizable>
+            <HeaderCell>Carteira</HeaderCell>
+            <WalletCell />
+          </Column>
+
+          <Column width={150} resizable>
+            <HeaderCell>Número de NFTs</HeaderCell>
+            <Cell dataKey="miners" />
+          </Column>
+        </Table>
+      </div>
+
+      <div>
+        <h2>Top 50 Carteiras por LF minado</h2>
+        <Table data={lfQuantity} width={650} height={400}>
+          <Column width={70} resizable>
+            <HeaderCell>Rank</HeaderCell>
+            <Cell dataKey="id" />
+          </Column>
+
+          <Column width={420} fixed resizable>
+            <HeaderCell>Carteira</HeaderCell>
+            <WalletCell />
+          </Column>
+
+          <Column width={160} resizable>
+            <HeaderCell>Quantidade Minada</HeaderCell>
+            <LFCell />
+          </Column>
+        </Table>
+      </div>
+
+      <div>
+        <h2>Top 10 Carteiras por Mineiros Lendários</h2>
+        <Table data={fiveStars} width={650} height={400}>
+          <Column width={70} resizable>
+            <HeaderCell>Rank</HeaderCell>
+            <Cell dataKey="id" />
+          </Column>
+
+          <Column width={420} fixed resizable>
+            <HeaderCell>Carteira</HeaderCell>
+            <WalletCell />
+          </Column>
+
+          <Column width={160} resizable>
+            <HeaderCell>Número de NFTs</HeaderCell>
+            <Cell dataKey="miners" />
+          </Column>
+        </Table>
+      </div>
+
+      <div>
+        <h2>Top 10 Carteiras por Mineiros Raros</h2>
+        <Table data={fourStars} width={650} height={400}>
+          <Column width={70} resizable>
+            <HeaderCell>Rank</HeaderCell>
+            <Cell dataKey="id" />
+          </Column>
+
+          <Column width={420} fixed resizable>
+            <HeaderCell>Carteira</HeaderCell>
+            <WalletCell />
+          </Column>
+
+          <Column width={160} resizable>
+            <HeaderCell>Número de NFTs</HeaderCell>
+            <Cell dataKey="miners" />
+          </Column>
+        </Table>
+      </div>
+
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          width={500}
+          height={300}
+          data={chartData}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,92 @@
+import data from './data.json';
+import minersByWallet from './minersByWallet.json';
+
+const rarityKey = [
+  '',
+  'oneStar',
+  'twoStars',
+  'threeStars',
+  'fourStars',
+  'fiveStars',
+  'sixStars',
+];
+
+const dataGenerator = () => {
+  const numberOfPlayers = Object.keys(minersByWallet).length;
+  const numberOfNFTs = data.length;
+
+  // quem tem mais mineiros? e quantos tem?
+  // ========================================================================================
+  const sortedByWalletNFTQuantity = Object.keys(minersByWallet).map((key) => {
+    const miners = minersByWallet[key];
+
+    return { wallet: key, miners };
+  }).sort((a, b) => b.miners.length - a.miners.length);
+  // ========================================================================================
+
+  // quem já farmou mais LF? e quanto foi?
+  // ========================================================================================
+  const sortedByMinedQuantity = Object.keys(minersByWallet).map((key) => {
+    const miners = minersByWallet[key];
+    const totalMined = miners.reduce((total, miner) => total + miner.totalMined, 0);
+
+    return { wallet: key, miners, totalMined };
+  }).sort((a, b) => b.totalMined - a.totalMined);
+  // ========================================================================================
+
+  // quem tem mais mineiros raros?
+  // ========================================================================================
+  const sortedByNFTRarityQuantity = Object.keys(minersByWallet).map((key) => {
+    const miners = minersByWallet[key];
+    const sixStar = miners.filter((miner) => miner.rarity === 6).length;
+    const fiveStar = miners.filter((miner) => miner.rarity === 5).length;
+    const fourStar = miners.filter((miner) => miner.rarity === 4).length;
+
+    return { wallet: key, miners, sixStar, fiveStar, fourStar };
+  }).sort((a, b) => b.fiveStar - a.fiveStar);
+  // ========================================================================================
+
+  // quantas contas tem pelo menos 10 mineiros?
+  // ========================================================================================
+  const numberOfFullLandWallets = Object.keys(minersByWallet).reduce((total, key) => {
+    const miners = minersByWallet[key];
+
+    return miners.length >= 10 ? total + 1 : total;
+  }, 0);
+  // ========================================================================================
+
+  // quantos LF já foi minado no total?
+  // ========================================================================================
+  const totalMinedLF = data.reduce((total, miner) => {
+    const totalMined = miner?.totalMined;
+
+    return total + totalMined;
+  }, 0);
+  // ========================================================================================
+
+  // quantos mineiros de cada raridade já saíram?
+  // ========================================================================================
+  const totalNFTByRarity = data.reduce((obj, miner) => {
+    const rarity = miner?.rarity;
+    const key = rarityKey[rarity];
+
+    return {
+      ...obj,
+      [key]: obj[key] + 1,
+    };
+  }, { sixStars: 0, fiveStars: 0, fourStars: 0, threeStars: 0, twoStars: 0, oneStar: 0 });
+  // ========================================================================================
+
+  return {
+    numberOfPlayers,
+    numberOfNFTs,
+    sortedByWalletNFTQuantity,
+    sortedByMinedQuantity,
+    sortedByNFTRarityQuantity,
+    numberOfFullLandWallets,
+    totalMinedLF,
+    totalNFTByRarity,
+  };
+};
+
+export default dataGenerator();
